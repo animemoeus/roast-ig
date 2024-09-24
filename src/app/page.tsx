eb463b24042ebaa777064ab74abb4460a0b96ffc
Arter Tendean <arter.tendean.07@gmail.com>
2024-09-24 21:30:58 +0700

    Clean the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ReCaptchaProvider, ReCaptcha } from "next-recaptcha-v3";
-import { useReCaptcha } from "next-recaptcha-v3";
+import { getInstagramUserData } from "@/utils";
 
 export default function Page() {
   const [username, setUsername] = useState<string>("");
@@ -12,20 +12,36 @@ export default function Page() {
 
   const handleRoasting = () => {
     if (!username) {
-      alert('Username ga boleh kosong anjirr!')
+      alert("Username ga boleh kosong anjirr!");
+      return;
     }
-  }
 
+    setIsLoading(true);
+    getInstagramUserData(username, recaptchaToken ?? "")
+      .then((result) => {
+        setRoastingText(result.roasting_text);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert("Lahh, error anjirr! Coba lagi ya, hehehe...");
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <>
       <ReCaptchaProvider reCaptchaKey="6Ldrbk0qAAAAAKBNvSqKUxRd1jnfaXsyo_Is6Eqv">
-        <div className="p-3 h-screen">
+        <div className="p-4 min-h-screen ">
           <div className="mx-auto h-full border-black border-2 rounded-md hover:shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
             <article className="w-full h-full">
-              <div className="px-4 py-2 text-left h-full">
-                <p className="text-base mb-4">May 15th, 2023</p>
-                <h1 className="text-[32px] mb-4">hmmm...</h1>
+              <div className="mb-3 px-4 py-2 text-left h-full">
+                <p className="text-base mb-4">hehehe...</p>
+
+                <h1 className="text-[32px] text-center mb-4">Roasting IG</h1>
+
                 <div className="flex flex-col sm:flex-row gap-1.5">
                   <input
                     className="basis-9/12 w-full border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] rounded-md"
@@ -39,8 +55,14 @@ export default function Page() {
                   >
                     {isLoading ? "Loading..." : "Roast! ☠️"}
                   </button>
-                  <ReCaptcha onValidate={token => { setRecaptchaToken(token) }} action="page_view" />
+                  <ReCaptcha
+                    onValidate={(token) => {
+                      setRecaptchaToken(token);
+                    }}
+                    action="page_view"
+                  />
                 </div>
+
                 <p className="mt-4 text-2xl mb-4">{roastingText}</p>
               </div>
             </article>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,22 @@
-import { useEffect } from "react";
-
-async function getInstagramUserData(username: string,recaptchaToken:string) {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Token ${process.env.ANIMEMOE_API_KEY}`);
-
-  console.log(username,recaptchaToken)
+async function getInstagramUserData(username: string, recaptchaToken: string) {
+  console.log(username, recaptchaToken);
   const requestOptions = {
     method: "GET",
-    headers: myHeaders,
   };
 
-
   try {
-    console.log(recaptchaToken)
+    console.log(recaptchaToken);
+    console.log(username, recaptchaToken);
     const response = await fetch(
       `https://api.animemoe.us/instagram/roasting/${username}/?captcha=${recaptchaToken}`,
       requestOptions,
     );
     const result = await response.json();
-    console.log(result)
+    console.log(result);
     return result;
   } catch (error) {
-    console.error(error);
     throw error;
   }
 }
 
-
-export { getInstagramUserData};
+export { getInstagramUserData };
