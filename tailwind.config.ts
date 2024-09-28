a3ce1ab6f6000c2926196fade1cfdb730b374fc3
Arter Tendean <arter.tendean.07@gmail.com>
2024-09-29 03:05:34 +0700

    Adding brutalism config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,40 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        main: "#bd52eb",
+        mainAccent: "#a81be4", // not needed for shadcn components
+        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+
+        // light mode
+        bg: "#e3dff2",
+        text: "#000",
+        border: "#000",
+
+        // dark mode
+        darkBg: "#1D1F27",
+        darkText: "#eeefe9",
+        darkBorder: "#000",
+        secondaryBlack: "#1b1b1b", // opposite of plain white, not used pitch black because borders and box-shadows are that color
+      },
+      borderRadius: {
+        base: "4px",
+      },
+      boxShadow: {
+        light: "6px 6px 0px 0px #000",
+        dark: "6px 6px 0px 0px #000",
+      },
+      translate: {
+        boxShadowX: "6px",
+        boxShadowY: "6px",
+        reverseBoxShadowX: "-6px",
+        reverseBoxShadowY: "-6px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
